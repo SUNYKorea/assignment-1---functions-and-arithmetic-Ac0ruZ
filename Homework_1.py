c7c951ed4b43b98a1faf00d62986ce887df2eada
taesik yoon <taesik.yoon@stonybrook.edu>
2023-03-28 18:19:06 +0900

    Taesik Yoon - Homework 1

--- Homework_1.py
+++ Homework_1.py
@@ -1,5 +1,5 @@
-# Name:
-# SBUID: 
+# Name: Taesik Yoon
+# SBUID: 115391860
 
 # Remove the ellipses (...) when writing your solutions.
 
@@ -8,10 +8,20 @@
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
 def fahrenheit2celsius(fahrenheit): 
-   ...
+   return (fahrenheit - 32) * 5 / 9
 
 def what_to_wear(celsius):
-   ...
+   if celsius < -10:
+       print ('Wear Puffy jacket')
+   elif celsius>= -10 and celsius < 0:
+       print ('Wear Scarf')
+   elif celsius >= 0 and celsius < 10:
+       print ('Wear Sweater')
+   elif celsius >= 10 and celsius < 20:
+       print ('Wear Light jacket')
+   else:
+       print ('Wear T-shirt')
+       
 
 # ---------------------------- Exercise II --------------------------------------
 # ----------------- Area and perimeter of a triangle  ---------------------------
@@ -19,13 +29,16 @@ def what_to_wear(celsius):
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-    ...
+    return abs((((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x1 * y3) + (x2 * y1) + (x3 * y2))) / 2)
 
 def euclidean_distance(x1, y1, x2, y2):
-    ...
+    return ((((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5)
 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    ...
+    s1 = euclidean_distance(x1,y1,x2,y2)
+    s2 = euclidean_distance(x2,y2,x3,y3)
+    s3 = euclidean_distance(x3,y3,x1,y1)
+    return s1 + s2 + s3
 
 
 # ---------------------------- Exercise III -------------------------------------
@@ -34,13 +47,17 @@ def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
 
 
 def deg2rad(deg):
-    ...
+    import math
+
+    return math.pi * (deg / 180)
 
 def apothem(number_sides, length_side):
-   ...
+   import math
+
+   return length_side / (2 * math.tan(deg2rad(180 / number_sides)))
 
 def polygon_area(number_sides, length_side):
-   ...
+   return (number_sides * length_side * apothem(number_sides, length_side)) / 2
 
 
 # ---------------------------- Test -------------------------------------
